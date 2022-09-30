14b2611138aa3be3c477cb16be0e4957e281c10f
juanfed <juanfed12345@gmail.com>
2022-09-30 15:12:36 -0500

    Job de imprecion

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module cron-golang
 
 go 1.19
+
+require (
+	github.com/robfig/cron v1.2.0 // indirect
+	github.com/robfig/cron/v3 v3.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	var c = cron.New()
+
+	defer c.Stop()
+
+	c.AddFunc("* * * * * *",
+		func() {
+			fmt.Println("Me muestro cada segundo")
+		},
+	)
+
+	c.Start()
 
 }
